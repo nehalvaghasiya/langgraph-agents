0bf468a0ff0d7c55d95f39b4c8a2ef7488c6413e
Nehal Vaghasiya <nehal.vaghasiya777@gmail.com>
2025-07-18 23:38:09 +0200

    added google search api

---
+++ src/infra/api/google_search.py
@@ -0,0 +1,9 @@
+from langchain_google_community import GoogleSearchAPIWrapper, GoogleSearchRun
+from core.utils.env import get_env
+
+def get_google_search():
+    wrapper = GoogleSearchAPIWrapper(
+        google_api_key=get_env("SERPAPI_API_KEY"),
+        google_cse_id=get_env("GOOGLE_CSE_ID")
+    )
+    return GoogleSearchRun(api_wrapper=wrapper)
