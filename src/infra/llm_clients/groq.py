06f6ad58d54663ed36a3f24db08bd5f028165e6f
Nehal Vaghasiya <nehal.vaghasiya777@gmail.com>
2025-07-18 23:37:48 +0200

    added llm client

---
+++ src/infra/llm_clients/groq.py
@@ -0,0 +1,11 @@
+from langchain_openai import ChatOpenAI
+from core.utils.env import get_env
+
+def get_llm():
+    return ChatOpenAI(
+        model_name="meta-llama/llama-4-scout-17b-16e-instruct",
+        temperature=0.7,
+        max_tokens=4096,
+        openai_api_key=get_env("OPENAI_API_KEY"),
+        openai_api_base="https://api.groq.com/openai/v1",
+    )
