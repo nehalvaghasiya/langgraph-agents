28b0123cef7a457c3c76377b303608c927130629
Nehal Vaghasiya <nehal.vaghasiya777@gmail.com>
2025-07-19 23:51:29 +0200

    added docstring to env

--- src/core/utils/env.py
+++ src/core/utils/env.py
@@ -4,4 +4,13 @@ import os
 load_dotenv()
 
 def get_env(key: str, default: str = "") -> str:
+    """Get Env Variables.
+
+    Args:
+        key (str): Name of env.
+        default (str, optional): Key. Defaults to "".
+
+    Returns:
+        str: Return value of Variable.
+    """
     return os.getenv(key, default)
