68443eb2d2525c0e08eb0a8cea28fc8f742e6a2e
Nehal Vaghasiya <78668871+nehalvaghasiya@users.noreply.github.com>
2025-08-10 23:59:17 +0200

    google_search.py updated

--- src/infra/api/google_search.py
+++ src/infra/api/google_search.py
@@ -2,7 +2,7 @@ from langchain_google_community import GoogleSearchAPIWrapper, GoogleSearchRun
 from core.utils.env import get_env
 
 def get_google_search():
-    """Get Instance of Google Search."""
+    """Get Instance of Google Search API wrapper."""
     wrapper = GoogleSearchAPIWrapper(
         google_api_key=get_env("SERPAPI_API_KEY"),
         google_cse_id=get_env("GOOGLE_CSE_ID")
