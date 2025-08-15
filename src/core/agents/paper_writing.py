a3c722c87bf75657c5d147abd9e7e08d9e4aa6a1
Nehal Vaghasiya <nehal.vaghasiya777@gmail.com>
2025-08-15 17:30:25 +0200

    added paper writing agent

---
+++ src/core/agents/paper_writing.py
@@ -0,0 +1,48 @@
+
+from typing import Literal
+from langgraph.graph import StateGraph, START
+from langchain_core.messages import HumanMessage
+from langgraph.types import Command
+
+from core.agents.chart_generator import ChartGeneratorAgent
+from core.agents.doc_writer import DocWriterAgent
+from core.agents.note_taker import NoteTakerAgent
+from core.agents.superwiser import SupervisorAgent
+
+from core.supervisor import State as SupervisorState, make_supervisor_node
+
+
+class PaperWritingTeamAgent:
+    def __init__(self, model):
+        self.model = model
+        # Define members as sub-agents
+        self.doc_writer = DocWriterAgent(model)
+        self.note_taker = NoteTakerAgent(model)
+        self.chart_generator = ChartGeneratorAgent(model)
+        self.members = ["doc_writer", "note_taker", "chart_generator"]
+        self.supervisor = SupervisorAgent(model, self.members)
+        # Build team graph
+        graph = StateGraph(SupervisorState)  # Use SupervisorState from your code
+        graph.add_node("supervisor", self.supervisor_node)
+        graph.add_node("doc_writer", self.doc_writer_node)
+        graph.add_node("note_taker", self.note_taker_node)
+        graph.add_node("chart_generator", self.chart_generator_node)
+        graph.add_edge(START, "supervisor")
+        # Conditional edges would be added based on supervisor routing (adapt from make_supervisor_node)
+        self.graph = graph.compile()
+
+    def supervisor_node(self, state: SupervisorState) -> Command[Literal["doc_writer", "note_taker", "chart_generator"]]:
+        # Use your make_supervisor_node logic here
+        return make_supervisor_node(self.model, self.members)(state)
+
+    def doc_writer_node(self, state: SupervisorState) -> Command[Literal["supervisor"]]:
+        result = self.doc_writer.graph.invoke(state)
+        return Command(update={"messages": [HumanMessage(content=result["messages"][-1].content, name="doc_writer")]}, goto="supervisor")
+
+    def note_taker_node(self, state: SupervisorState) -> Command[Literal["supervisor"]]:
+        result = self.note_taker.graph.invoke(state)
+        return Command(update={"messages": [HumanMessage(content=result["messages"][-1].content, name="note_taker")]}, goto="supervisor")
+
+    def chart_generator_node(self, state: SupervisorState) -> Command[Literal["supervisor"]]:
+        result = self.chart_generator.graph.invoke(state)
+        return Command(update={"messages": [HumanMessage(content=result["messages"][-1].content, name="chart_generator")]}, goto="supervisor")
