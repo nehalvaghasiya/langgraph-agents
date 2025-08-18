e47d806a55875872b6294544d7e9818a5f809c82
Nehal Vaghasiya <78668871+nehalvaghasiya@users.noreply.github.com>
2025-08-18 23:50:30 +0200

    web_search.py updated

--- src/core/agents/web_search.py
+++ src/core/agents/web_search.py
@@ -3,6 +3,7 @@ from infra.api.google_search import get_google_search
 
 
 class SearchAgent(BaseAgent):
+    """Search Agent class to search on Internet."""
     def __init__(self, model, tools=[get_google_search()], system=""):
         system = system or "You are a search assistant. Use the web search tool to look up information. Don't ask follow-up questions."
         super().__init__(model, tools, system)
