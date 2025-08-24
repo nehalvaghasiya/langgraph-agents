37078c7f326761d828a61d69407776ee5194d5d1
Nehal Vaghasiya <78668871+nehalvaghasiya@users.noreply.github.com>
2025-08-24 22:53:00 +0200

    Initialized DocWriterAgent

--- src/core/agents/doc_writer.py
+++ src/core/agents/doc_writer.py
@@ -5,5 +5,6 @@ from core.tools.document_io import edit_document, read_document, write_document
 class DocWriterAgent(BaseAgent):
     """Agent class for writing Document."""
     def __init__(self, model, tools=[write_document, edit_document, read_document], system=""):
+        """Initialize DocWriterAgent."""
         system = system or "You can read, write and edit documents based on note-taker's outlines. Don't ask follow-up questions."
         super().__init__(model, tools, system)
