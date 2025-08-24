f79776c37e3bc09bfc809c8e39e7635fcd57ee1d
Nehal Vaghasiya <78668871+nehalvaghasiya@users.noreply.github.com>
2025-08-24 22:51:56 +0200

    added docstring for DocWriterAgent in doc_writer.py

--- src/core/agents/doc_writer.py
+++ src/core/agents/doc_writer.py
@@ -3,6 +3,7 @@ from core.tools.document_io import edit_document, read_document, write_document
 
 
 class DocWriterAgent(BaseAgent):
+    """Agent class for writing Document."""
     def __init__(self, model, tools=[write_document, edit_document, read_document], system=""):
         system = system or "You can read, write and edit documents based on note-taker's outlines. Don't ask follow-up questions."
         super().__init__(model, tools, system)
