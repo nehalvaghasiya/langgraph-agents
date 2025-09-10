bf2cf32db9f23172cbef7bf3798875370b77afdd
Nehal Vaghasiya <78668871+nehalvaghasiya@users.noreply.github.com>
2025-09-10 23:56:05 +0200

    docs for WebScraperAgent

--- src/core/agents/web_scraper.py
+++ src/core/agents/web_scraper.py
@@ -3,6 +3,7 @@ from core.tools.scrape import scrape_webpages
 
 
 class WebScraperAgent(BaseAgent):
+    """Create Agent for Web scraping."""
     def __init__(self, model, tools=[scrape_webpages], system=""):
         system = system or "You are a web scraper. Use tools to scrape webpages. Don't ask follow-up questions."
         super().__init__(model, tools, system)
