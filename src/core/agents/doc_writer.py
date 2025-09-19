5b06b4ca649fe045b64c436baa4bd91200344187
Nehal Vaghasiya <nehal.vaghasiya777@gmail.com>
2025-09-19 23:50:50 +0200

    ruff check for doc writer

--- src/core/agents/doc_writer.py
+++ src/core/agents/doc_writer.py
@@ -1,10 +1,21 @@
+from langchain_core.language_models import BaseChatModel
+
 from core.agents.base import BaseAgent
 from core.tools.document_io import edit_document, read_document, write_document
 
 
 class DocWriterAgent(BaseAgent):
     """Agent class for writing Document."""
-    def __init__(self, model, tools=[write_document, edit_document, read_document], system=""):
+
+    def __init__(
+        self,
+        model: BaseChatModel,
+        tools: list = [write_document, edit_document, read_document],
+        system: str = "",
+    ):
         """Initialize DocWriterAgent."""
-        system = system or "You can read, write and edit documents based on note-taker's outlines. Don't ask follow-up questions."
+        system = (
+            system
+            or "You can read, write and edit documents based on note-taker's outlines. Don't ask follow-up questions."
+        )
         super().__init__(model, tools, system)
