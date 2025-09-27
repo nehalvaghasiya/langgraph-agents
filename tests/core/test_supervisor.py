c8e38e4cf783fee67832c70c5a009756393215cc
Nehal Vaghasiya <nehal.vaghasiya777@gmail.com>
2025-09-27 22:18:42 +0200

    added DummyLLM in tests for supervisor

---
+++ tests/core/test_supervisor.py
@@ -0,0 +1,21 @@
+import pytest
+from types import SimpleNamespace
+from core.supervisor import State, make_supervisor_node
+from langgraph.types import Command
+
+class DummyLLM:
+    def __init__(self, next_value=None, raise_exc=False, missing_next=False):
+        self.next_value = next_value
+        self.raise_exc = raise_exc
+        self.missing_next = missing_next
+
+    def with_structured_output(self, Router):
+        return self
+    
+    def invoke(self, messages):
+        if self.raise_exc:
+            raise Exception("LLM error")
+        if self.missing_next:
+            return SimpleNamespace()  # No 'next' attribute
+        return SimpleNamespace(next=self.next_value)
+
