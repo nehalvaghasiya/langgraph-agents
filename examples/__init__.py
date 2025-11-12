ae745ef16083d9f07aa182d6729aad7cdba69df4
Nehal Vaghasiya <nehal.vaghasiya777@gmail.com>
2025-11-12 23:46:28 +0100

    added init in examples dir

---
+++ examples/__init__.py
@@ -0,0 +1,15 @@
+"""Examples for LangGraph Agents.
+
+This package contains example scripts demonstrating how to use different agents
+from the LangGraph agents framework.
+
+Example modules:
+    - doc_writer: Using DocWriterAgent to generate documents
+    - paper_writing_team: Using PaperWritingTeamAgent for complex writing tasks
+    - research_team: Using ResearchTeamAgent to research topics
+    - rag: Using RagAgent for retrieval-augmented generation
+    - web_search: Using SearchAgent for web searches
+    - note_taker: Using NoteTakerAgent to take notes
+    - chart_generator: Using ChartGeneratorAgent to create charts
+    - web_scraper: Using WebScraperAgent to scrape web content
+"""
